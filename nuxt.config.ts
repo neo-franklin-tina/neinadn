871b1f18eb6dea2ec2263432a93ea5ef86a403b9
teena-dot-dev <ftina624@gmail.com>
2022-02-05 01:18:45 -0600

    Done

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,19 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    meta: {
+        title: '#neinadn',
+        description: 'Not Every Idea Needs A Domain Name',
+        link: [
+            { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+            { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Bungee&display=swap' }
+        ]
+    },
+    css: [
+        '~/assets/css/app.css'
+    ],
+    components: [
+        '~/components'
+    ]
 })
