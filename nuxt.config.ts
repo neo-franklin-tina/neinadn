8ad9f35bd7177ff514e0832c1d9f2cece7c154bf
teena-dot-dev <ftina624@gmail.com>
2022-02-05 01:46:43 -0600

    added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,12 @@ export default defineNuxtConfig({
         link: [
             { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
             { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
-            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Bungee&display=swap' }
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Bungee&display=swap' },
+
+            { rel: 'apple-touch-icon', sizes: '180x180', href: '/img/apple-touch-icon.png' },
+            { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/img/favicon-32x32.png' },
+            { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/img/favicon-16x16.png' },
+            { rel: 'manifest', href: '/img/site.webmanifest' },
         ]
     },
     css: [
